636d54ad4ad28cf8859f20d3f744e62972e945f9
sawal27 <36143663+sawal27@users.noreply.github.com>
2018-04-13 12:47:25 +0800

    Update Siblings1.php

--- Siblings1.php
+++ Siblings1.php
@@ -11,22 +11,22 @@ body{
 </style>
 </head>
 <body>
-<h1><center>Alenie</h1><center>
-<center><img src="alenie.jpg" height="200" width="200"></center>
+<h1><center>Shanlie</h1><center>
+<center><img src="shanlie.jpg" height="200" width="200"></center>
 <p><a href="content.php" target="content">HOME</a></p>
 <h1><center>INFORMATION</h1></center>
-<p><b>First Name:</b>Alenie</p>
-<p><b>Middle Name:</b>Flores</p>
+<p><b>First Name:</b>Shanlie</p>
+<p><b>Middle Name:</b>flores</p>
 <p><b>Last Name:</b>Sawal</p>
 <p><b>Address:</b>Aning Cadulawan Minglanillia Cebu</p>
-<p><b>Age:</b>19 yrs Old</p>
+<p><b>Age:</b>14 yrs Old</p>
 <p><b>Sex:</b>Female</p>
 <p><b>Status:</b>single</p>
 <p><b>Religion:</b>Roman Catholic</p>
 <p><b>Citizenship:</b>Filipino</p>
-<p><b>Email Address:</b>alenie.sawal@facebook.com</p>
-<p><b>Contact No.</b>09156443738</p><br>
-<p><b>School:CTU-NAGA CAMPUS</p><br>	
-<p><b>Favorite Song:</b>True colors</p>
+<p><b>Email Address:</b>shan.sawal@facebook.com</p>
+<p><b>Contact No.</b>091556576838</p><br>
+<p><b>School:PAP NAGA</p><br>	
+<p><b>Favorite Song:</b>One thing</p>
 </body>
 </html>
