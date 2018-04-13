878ad86ccdd505edc77756d7367dc943eb9c05ee
sawal27 <36143663+sawal27@users.noreply.github.com>
2018-04-13 12:12:35 +0800

    Update Cousin1.php

--- Cousin1.php
+++ Cousin1.php
@@ -5,13 +5,14 @@
 body{
     font-family:verdana,arial,san-serif;
     margin:30px;
-    background-color:Brown;
+    background-color:#191970;
    }
 
 </style>
 </head>
 <body>
 <h1><center>COUSIN</h1><center>
+<center><img src="jayvee.jpg" height="200" width="200"></center>
 <p><a href="content.php" target="content">HOME</a></p>
 <h1><center>INFORMATION</h1></center>
 <p><b>First Name:</b>Jayvee</p>
